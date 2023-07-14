8b280482866f5f9024dd85685a4b168238791a82
Bob Witmer <bwitmer@golden.net>
2023-07-14 10:58:57 -0400

    Initial Commit

---
+++ Math-Tutor/Math_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_TutorApp.swift
+//  Math-Tutor
+//
+//  Created by Bob Witmer on 2023-07-14.
+//
+
+import SwiftUI
+
+@main
+struct Math_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
